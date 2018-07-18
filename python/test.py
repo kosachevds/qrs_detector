0955ffa380ad7351190fecc61aaf50a5dae69ce2
Даниил Косачев <kdaniil@MCS>
2018-07-18 13:19:42 +0300

    Some fixes

--- python/test.py
+++ python/test.py
@@ -5,13 +5,13 @@ import detection
 
 def main():
     current_dir = os.path.dirname(__file__)
-    with open(os.path.join(current_dir, "data", "input")) as in_file:
+    with open(os.path.join(current_dir, "..", "data", "input")) as in_file:
         values = in_file.read().split()
         input_signal = [float(x) for x in values]
     result = detection.detect(input_signal, 2000)
     pp.plot(input_signal)
-    for x in result:
-        pp.axvline(x, color="r")
+    for peak in result:
+        pp.axvline(peak, color="r")
     pp.show()
 
 if __name__ == '__main__':
