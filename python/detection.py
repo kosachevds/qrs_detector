f70a03733efd5739d1b0ad8df5306f91c634ac20
Даниил Косачев <kdaniil@MCS>
2018-07-18 13:21:22 +0300

    Change thresholding

--- python/detection.py
+++ python/detection.py
@@ -1,15 +1,29 @@
 import numpy as np
+# from matplotlib import pyplot as pp
 
 _WINDOW_SEC = 0.150
 _MIN_RR = 0.2
 
 
 def detect(signal, rate):
+    delay = 0
     filtered = _low_pass_filter(signal)
+    delay += 6
     filtered = _high_pass_filter(filtered)
+    delay += 16
+    # pp.figure(0)
+    # pp.title("Filtered")
+    # pp.plot(filtered)
     integrated = _squared_derivative(filtered)
     integrated = _window_integration(integrated, int(_WINDOW_SEC * rate))
-    return _thresholding(signal, filtered, integrated, rate)
+    delay += round(_WINDOW_SEC * rate) / 2
+    # pp.figure(1)
+    # pp.title("Integrated")
+    # pp.plot(integrated)
+    # pp.figure(2)
+    # return _thresholding(signal, filtered, integrated, rate)
+    indecies = [x - delay for x in _new_thresholding(integrated, rate)]
+    return indecies
 
 
 def _low_pass_filter(signal):
@@ -83,44 +97,62 @@ def _thresholding(signal, filtered, integrated, rate):
         if integrated[i] >= threshold2:
             if i - peaks[-1] >= _MIN_RR * rate:
                 peaks.append(i)
-        #TODO: correct first
+        # TODO: correct first
     return peaks[1:]
 
-def _new_thresholding(signal, integrated, rate):
+
+def _new_thresholding(integrated, rate):
+    min_interval = int(_MIN_RR * rate)
+    peak_indicies = _find_peaks(np.array(integrated), limit=0.35,
+                                spacing=min_interval)
     spki = 0
     npki = 0
-    peaks = [0]
-    threshold = integrated[0]
-    i = 0
-    while i < len(integrated):
-        index = _next_cadidate(signal, integrated, i)
-        if index < 0:
-            i += 1
+    peaks = []
+    last_peak = 0
+    threshold = 0
+    for index in peak_indicies:
+        if last_peak > 0 and index - last_peak < min_interval:
             continue
-        peaki = integrated[index]
-        if peaki < threshold:
-            npki = 0.875 * npki + 0.125 * peaki
+        value = integrated[index]
+        if value < threshold:
+            npki = 0.875 * npki + 0.125 * value
         else:
-            spki = 0.875 * spki + 0.125 * peaki
             peaks.append(index)
-            i = index + int(_MIN_RR * rate)
+            spki = 0.875 * spki + 0.125 * value
+            last_peak = index
         threshold = npki + 0.25 * (spki - npki)
-        i += 1
-    return peaks[1:]
+    return peaks
 
 
-def _next_cadidate(signal, integrated, start_index):
-    candidate = start_index
-    value = signal[start_index]
-    j = start_index + 1
-    while j < len(integrated) and integrated[j] > integrated[j - 1]:
-        if signal[j] < value:
-            j += 1
-            continue
-        if signal[j - 1] < signal[j] and signal[j + 1] < signal[j]:
-            candidate = j
-            value = signal[j]
-        j += 1
-    if candidate == start_index:
-        return -1
-    return candidate
+def _find_peaks(data, spacing=1, limit=None):
+    """
+    Janko Slavic peak detection algorithm and implementation.
+    https://github.com/jankoslavic/py-tools/tree/master/findpeaks
+    Finds peaks in `data` which are of `spacing` width and >=`limit`.
+    :param ndarray data: data
+    :param float spacing: minimum spacing to the next peak (should be 1 or more)
+    :param float limit: peaks should have value greater or equal
+    :return array: detected peaks indexes array
+    """
+    len_ = data.size
+    x = np.zeros(len_ + 2 * spacing)
+    x[:spacing] = data[0] - 1.e-6
+    x[-spacing:] = data[-1] - 1.e-6
+    x[spacing:spacing + len_] = data
+    peak_candidate = np.zeros(len_)
+    peak_candidate[:] = True
+    for s in range(spacing):
+        start = spacing - s - 1
+        h_b = x[start: start + len_]  # before
+        start = spacing
+        h_c = x[start: start + len_]  # central
+        start = spacing + s + 1
+        h_a = x[start: start + len_]  # after
+        peak_candidate = np.logical_and(peak_candidate,
+                                        np.logical_and(h_c > h_b, h_c > h_a))
+
+    ind = np.argwhere(peak_candidate)
+    ind = ind.reshape(ind.size)
+    if limit is not None:
+        ind = ind[data[ind] > limit]
+    return ind
