fc4eaa4e186b70072aef57604ad336727baeac06
Даниил Косачев <kdaniil@MCS>
2019-03-14 15:47:22 +0300

    Removed function '_find_peaks'

--- python/detection.py
+++ python/detection.py
@@ -129,38 +129,6 @@ def _find_local_max(values):
     return peak_index
 
 
-def _find_peaks(data, spacing=1, limit=None):
-    """
-    Finds peaks in `data` which are of `spacing` width and >=`limit`.
-    :param ndarray data: data
-    :param float spacing: minimum spacing to the next peak (should be 1 or more)
-    :param float limit: peaks should have value greater or equal
-    :return array: detected peaks indexes array
-    """
-    data = np.array(data)
-    size = data.size
-    x = np.zeros(size + 2 * spacing)
-    x[:spacing] = data[0] - 1.e-6
-    x[-spacing:] = data[-1] - 1.e-6
-    x[spacing:spacing + size] = data
-    peak_candidate = np.zeros(size)
-    peak_candidate[:] = True
-    for s in range(spacing):
-        start = spacing - s - 1
-        h_b = x[start: start + size]  # before
-        start = spacing
-        h_c = x[start: start + size]  # central
-        start = spacing + s + 1
-        h_a = x[start: start + size]  # after
-        peak_candidate = np.logical_and(peak_candidate,
-                                        np.logical_and(h_c > h_b, h_c > h_a))
-    ind = np.argwhere(peak_candidate)
-    ind = ind.reshape(ind.size)
-    if limit is not None:
-        ind = ind[data[ind] > limit]
-    return ind
-
-
 def _find_peaks_(data, spacing, limit):
     size = len(data)
     x = [data[0] - 1.0e-6 for _ in range(spacing)]
