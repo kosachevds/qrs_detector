5a39cedf015d73d20b395276714d1505a9fc31ab
Даниил Косачев <kdaniil@MCS>
2019-03-20 14:48:49 +0300

    Added marks defines

--- c89/qrs_detection.h
+++ c89/qrs_detection.h
@@ -1,6 +1,9 @@
 #ifndef QRS_DETECTION_H
 #define QRS_DETECTION
 
+#define MARK_NO_QRS (0)
+#define MARK_QRS    (1)
+
 int DetectQrsPeaks(double const* signal, int size, char* result, double rate);
 
 #endif
